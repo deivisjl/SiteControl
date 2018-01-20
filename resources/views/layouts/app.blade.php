156178b2f4a7fba70b58f1745549386f32cf42e1
deivisjl <deivisjl@gmail.com>
2018-01-20 16:13:49 -0600

    Refactorizando vista y rutas
    Refactorizacion de la vista principal y rutas al estar logueado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,27 @@
               </button>
               <a class="navbar-brand" href="{{ url('/') }}">Site Control</a>
             </div>
+             <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                  <li class="active"><a href="{{ url('/') }}">Inicio</a></li>
+                </ul>
+
+                <!-- pull right-->
+                @if (!Auth::guest())
+                   <ul class="nav navbar-nav navbar-right">                    
+                    <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->email }} <span class="caret"></span></a>
+                      <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="{{ route('logout') }}">Logout</a></li>
+                    </li>
+                  </ul>
+                @endif  
+
+              </div><!--/.nav-collapse -->
          </div>
     </nav>
 
